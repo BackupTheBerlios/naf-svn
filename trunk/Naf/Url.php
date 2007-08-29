e25d557735a317bbb0268cce3d6d07ba4c55630b
vbolshov <vbolshov@bcf0c570-6435-0410-86cd-cb52d6d489dc>
2007-08-29 10:14:24 +0000

    take care of PHPSESSID

--- trunk/Naf/Url.php
+++ trunk/Naf/Url.php
@@ -1,12 +1,26 @@
 <?php
 
+/**
+ * Compose Naf-style URLs.
+ * Take care of SESSID when needed.
+ */
+
 class Naf_Url {
 	protected $_base = './';
+	protected $_appendSid;
 	
 	function __construct($base = null)
 	{
 		if (null !== $base)
 			$this->_base = $base;
+		
+		$sessionName = session_name();
+		if ((! isset($_COOKIE[$sessionName])) && 
+			(isset($_GET[$sessionName]) || isset($_POST[$sessionName])) && 
+			ini_get('session.use_trans_sid'))
+		{
+			$this->_appendSid = true;
+		}
 	}
 	
 	/**
@@ -20,6 +34,9 @@ class Naf_Url {
 			$action = Naf::currentAction();
 		
 		$params['ctrl'] = str_replace('/', '.', $action);
+		if ($this->_appendSid && $sid = session_id())
+			$params[session_name()] = $sid;
+		
 		return $this->_base . '?' . http_build_query($params, null, $separator);
 	}
 	
