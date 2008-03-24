bbd7d0aedd59029c4e6f9256a865a9b1229e5c0a
vbolshov <vbolshov@bcf0c570-6435-0410-86cd-cb52d6d489dc>
2008-03-24 09:31:04 +0000

    this class now extends Naf_Upload_Exception - not Exception

--- trunk/Naf/Upload/NoFileException.php
+++ trunk/Naf/Upload/NoFileException.php
@@ -1,6 +1,6 @@
 <?php
 
-class Naf_Upload_NoFileException extends Exception {
+class Naf_Upload_NoFileException extends Naf_Upload_Exception {
 	function __construct()
 	{
 		parent::__construct("No file has been uploaded");
