57a9b9461dea5dc09b835f34a8a96078556b1498
vbolshov <vbolshov@bcf0c570-6435-0410-86cd-cb52d6d489dc>
2008-04-07 08:33:47 +0000

    Progress subpackage migrated to naf::util

--- branches/php-5.3/naf/util/Progress.php
+++ branches/php-5.3/naf/util/Progress.php
@@ -7,9 +7,18 @@
  * process is not hung up)
  *
  * Naf_Progress is a simple backend for task-state monitor.
+ * 
+ * $Id$
+ * 
+ * @package naf::util
+ * @subpackage Progress
+ * @copyright Victor Bolshov <crocodile2u@gmail.com>
  */
 
-class Naf_Progress {
+namespace naf::util;
+use naf::util::Progress::Fault;
+
+class Progress {
 	/**
 	 * @var string
 	 */
@@ -71,15 +80,15 @@ class Naf_Progress {
 	{
 		if (! is_string($dir))
 		{
-			throw new Naf_ProgressBarBackend_Exception("Argument 1 is expected to be a string, " . gettype($dir) . " given");
+			throw new Fault("Argument 1 is expected to be a string, " . gettype($dir) . " given");
 		}
 		if (! is_dir($dir))
 		{
-			throw new Naf_ProgressBarBackend_Exception("$dir is not a directory");
+			throw new Fault("$dir is not a directory");
 		}
 		if (! is_writable($dir))
 		{
-			throw new Naf_ProgressBarBackend_Exception("$dir is not writable");
+			throw new Fault("$dir is not writable");
 		}
 		
 		$this->tmpdir = rtrim($dir, '/ ');
---
+++ branches/php-5.3/naf/util/Progress/Fault.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * naf::util::Progress::Fault
+ */
+
+namespace naf::util::Progress;
+
+class Fault extends Exception {
+	
+}
