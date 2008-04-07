74214725f0ebe0e451a2c045574b2adbfe46090c
vbolshov <vbolshov@bcf0c570-6435-0410-86cd-cb52d6d489dc>
2008-04-07 11:12:59 +0000

    initial import: naf-style controllers base class

---
+++ branches/php-5.3/naf/controller/Controller.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * This is the base class for a Naf-style MVC controller.
+ * 
+ * $Id$
+ * 
+ * @package naf::controller
+ * @copyright Victor Bolshov <crocodile2u@gmail.com>
+ */
+
+namespace naf::controller;
+
+abstract class Controller {
+	/**
+	 * @var array
+	 */
+	protected $postRequestHandlers;
+	
+	/**
+	 * @var Naf_Response
+	 */
+	protected $response;
+	
+	/**
+	 * Constructor. This method is marked final,
+	 * and it is a template-method (calls $this->setUp())
+	 */
+	final function __construct()
+	{
+		$this->response = Naf::response();
+		$this->setUp();
+		
+		$class = get_class($this);
+		foreach ($this->postRequestHandlers as $trigger => $handler)
+		{
+			if ((! $handler) || (is_string($handler) && ($handler == $class)))
+			{
+				$this->postRequestHandlers[$trigger] = $this;
+			}
+		}
+	}
+	
+	/**
+	 * YOUR SETUP HERE
+	 */
+	protected  function setUp()
+	{}
+	
+	/**
+	 * Run this controller. This method is marked final,
+	 * and it is a template-method (calls $this->doRun() and $this->render())
+	 */
+	final function run()
+	{
+		if (is_array($this->postRequestHandlers) && 
+			count($this->postRequestHandlers) && 
+			Naf::handlePost($this->postRequestHandlers))
+		{
+			return ;
+		}
+		
+		$this->doRun();
+		$this->render($this->response->getView());
+	}
+	
+	/**
+	 * Actually run this controller
+	 */
+	abstract function doRun();
+	/**
+	 * render view
+	 */
+	abstract function render();
+}
