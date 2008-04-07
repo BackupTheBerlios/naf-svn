0acbe7f1fe73aa8b5b0e9ee8a514dd198aa36f21
vbolshov <vbolshov@bcf0c570-6435-0410-86cd-cb52d6d489dc>
2008-04-07 09:14:43 +0000

    removed unit tests for old (deleted) classes

--- branches/php-5.3/unit_tests/config.example.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('TEST_TMP_DIR', dirname(__FILE__) . '/tmp/');
-define('TEST_FFMPEG', '/usr/local/bin/ffmpeg');
-define('TEST_MPLAYER', '/usr/local/bin/mplayer');
-define('TEST_MENCODER', '/usr/local/bin/mencoder');
